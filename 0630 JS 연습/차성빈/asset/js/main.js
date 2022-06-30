6fe81ed0533ae3b204acea73739f22f484852f61
PolarisS2 <62577967+PolarisS2@users.noreply.github.com>
2022-06-30 17:28:26 +0900

    Add files via upload

---
+++ 0630 JS 연습/차성빈/asset/js/main.js
@@ -0,0 +1,14 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "80%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("mySidebar").style.backgroundColor = "black";
+    document.getElementById("mySidebar").style.color="white";
+    document.getElementById("nav").style.display="none";
+  }
+  function closeNav() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("nav").style.display = "block";
+  
+    
+    
+  }
